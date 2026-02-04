eabdf3e73f0b3cf6c0d536869e6bfc5eae074521
Ranggadewa11 <95867224+Ranggadewa11@users.noreply.github.com>
2026-02-05 00:01:20 +0700

    Update flake.nix

--- Nix/flake.nix
+++ Nix/flake.nix
@@ -1,59 +1,55 @@
 {
-  description = "NixOS Configuration with Flakes, Home Manager, and Mango";
+  description = "Nixpant8";
 
   inputs = {
-    # Nixpkgs Unstable (Sesuai request)
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    mango.url = "github:DreamMaoMao/mango";
+    dms.url = "github:AvengeMedia/DankMaterialShell";
+    quickshell.url = "git+https://git.outfoxxed.me/quickshell/quickshell";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    nix-monitor.url = "github:antonjah/nix-monitor";
 
-    # Home Manager
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Mango (Window Manager)
-    mango = {
-      url = "github:DreamMaoMao/mango";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    dms = {
-      url = "github:AvengeMedia/DankMaterialShell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    # Quickshell (Jika diminta manual di dokumentasi)
-    quickshell = {
-      url = "git+https://git.outfoxxed.me/quickshell/quickshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, mango, dms, ... }: {
-    # Hostname kamu adalah "nixos" (sesuai configuration.nix baris 5)
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      
-      # Meneruskan input ke dalam modul agar bisa dipanggil
-      specialArgs = { inherit inputs; };
-      
-      modules = [
-        # Modul Sistem Utama
-        ./configuration.nix
 
-        # Modul Mango
+    # =========================
+    # NixOS (SYSTEM)
+    # =========================
+    nixosConfigurations.ax14r5 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+
+      modules = [
+        ./configuration.nix
         mango.nixosModules.mango
         dms.nixosModules.dank-material-shell
-
-         
-        # Modul Home Manager
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = { inherit inputs; };
-          home-manager.users.dewtf = import ./home-manager/home.nix;
-        }
       ];
     };
-  };
+
+    # =========================
+    # Home Manager (USER)
+    # =========================
+   # =========================
+# Home Manager (USER)
+# =========================
+homeConfigurations.dewtf =
+  home-manager.lib.homeManagerConfiguration {
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      config.allowUnfree = true;
+    };
+
+    extraSpecialArgs = { inherit inputs; };
+
+    modules = [
+      inputs.nix-monitor.homeManagerModules.default
+      ./home-manager/home.nix
+          ];
+        }; 
+     };
 }
